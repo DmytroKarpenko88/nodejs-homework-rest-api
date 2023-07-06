f2912aa01d6acda20f18d10a6bb67060650e0b89
Dmytro Karpenko <kardmitriy@gmail.com>
2023-07-06 14:16:59 +0300

    filter favorite

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,11 +1,27 @@
+const { HttpError } = require('../../helpers');
 const { Contact } = require('../../models/contact');
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, '-createdAt -updatedAt', {
+  const searchParams = {
+    owner,
+  };
+
+  // filter
+  const favoriteCondition = favorite === 'true' || favorite === 'false';
+  if (!favoriteCondition && favorite !== undefined) {
+    throw HttpError(400, 'Wrong "favorite" query parameter');
+  }
+  if (favoriteCondition) {
+    searchParams.favorite = favorite;
+  }
+
+  console.log(favoriteCondition, favorite);
+
+  const result = await Contact.find(searchParams, '-createdAt -updatedAt', {
     skip,
     limit,
   }).populate('owner', 'email');
