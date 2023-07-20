67a2c6acffd94d2bb9604c652eaef8614a5884a6
Dmytro Karpenko <kardmitriy@gmail.com>
2023-07-20 22:09:38 +0300

    update authenticte

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,7 +7,7 @@ const authenticate = async (req, res, next) => {
   const { authorization = '' } = req.headers;
   const [bearer, token] = authorization.split(' ');
 
-  if (bearer !== 'Bearer') {
+  if (bearer !== 'Bearer' || !token) {
     next(HttpError(401));
   }
 
@@ -15,7 +15,7 @@ const authenticate = async (req, res, next) => {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
 
-    if (!user) {
+    if (!user || !user.token || user.token !== token) {
       next(HttpError(401));
     }
 
